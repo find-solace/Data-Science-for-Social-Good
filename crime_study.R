fd94d5ddb097409dddad1551e1ab49af5bb40ce8
find-solace <solaceakello@gmail.com>
2023-12-05 10:36:42 +0300

    load datasets

--- crime_study.R
+++ crime_study.R
@@ -1,2 +1,5 @@
 library(tidyverse)
 library(lubridate)
+
+calls <- read_csv("https://raw.githubusercontent.com/veeralakrishna/Datacamp-Project-Solutions-R/master/Data%20Science%20for%20Social%20Good_%20Crime%20Study/datasets/downsample_police-department-calls-for-service.csv")
+incidents <- read_csv("https://raw.githubusercontent.com/kchoward48/DataCamp_Projects_in_R/main/downsample_police-department-incidents.csv")
